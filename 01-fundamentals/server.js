3fa343e1c881bb15c772b9a697e78e3e4562793f
Hung Bui <hungbui7690@gmail.com>
2023-03-01 06:36:20 -0700

    Use GraphQL Client to Query

--- 01-fundamentals/server.js
+++ 01-fundamentals/server.js
@@ -1,28 +1,29 @@
 /*
-  RESOLVER FUNCTIONS
-  -
-
+  QUERY LANGUAGE
+  - go to http://localhost:9000/ > will see the sandbox = one of GraphQL Client
+  - check pictures > graphql-client
 
 */
 
 import { ApolloServer, gql } from 'apollo-server'
 
 const typeDefs = gql`
+  # this is default bts
+  schema {
+    query: Query
+  }
+
   type Query {
     greeting: String
   }
 `
 
-// (1) create resolvers function
-// this needs to match typeDefs
-// > greeting now is a function that returns String
 const resolvers = {
   Query: {
     greeting: () => 'Hello World',
   },
 }
 
-// (2) create graphql server
 const server = new ApolloServer({ typeDefs, resolvers })
 const serverInfo = await server.listen({ port: 9000 })
 console.log(`Server is running at ${serverInfo.url}`)
